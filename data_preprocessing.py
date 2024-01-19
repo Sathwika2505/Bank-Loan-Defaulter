35900ff2959043ba8bc2cf26cc6ece0c4f029160
SathwikaGopishetty <111847859+SathwikaGopishetty@users.noreply.github.com>
2024-01-19 18:18:52 +0530

    fix visualisation

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -4,9 +4,9 @@ from loadingdata import loading_data
 def data_preprocess():
     data = loading_data()
     data =  data.drop(['ID', 'Payment Plan', 'Accounts Delinquent','Batch Enrolled','Employment Duration','Home Ownership'], axis = 1)
-    print(data.columns)
+    #print(data.columns)
     data['Funded Amount Investor'] = data['Funded Amount Investor'].astype('int')
-    print("----------------------data_preprocess------------------",data)
+    #print("----------------------data_preprocess------------------",data)
     return data
 
 data_preprocess()
--- datavisualization.py
+++ datavisualization.py
@@ -15,35 +15,39 @@ from PIL import Image
 a =[]
 def data_visualization():
     data = data_preprocess()
-    print("visualization data-------", data.head())
     cols = ['Grade','Sub Grade','Verification Status','Loan Title','Application Type','Initial List Status']
     labelencoder = LabelEncoder()
     for column in cols:
         data[column] = labelencoder.fit_transform(data[column])
         print(data[column])
+    num_col = ['Interest Rate','Debit to Income','Total Received Interest']
     col=list(data.columns)
     col.remove("Loan Status")
     print(col)
-    for i in col:
+    for i in num_col:
         fig = px.box(data, y=i)
         fig.update_layout(template='plotly_dark')
         #fig.update_layout(plot_bgcolor = "plotly_dark")
         fig.update_xaxes(showgrid=False,zeroline=False)
         fig.update_yaxes(showgrid=False,zeroline=False)
         # fig.show()
-        fig.write_image(f"{i}.jpg")
+        fig.write_image(f"boxplot for {i}.jpg")
         # a.append(fig)
-    for i in col:
+    for i in num_col:
         fig = ff.create_distplot([data[i].values],group_labels=[i])
         fig.update_layout(template='plotly_dark')
         #fig.update_layout(plot_bgcolor = "plotly_dark")
         fig.update_xaxes(showgrid=False,zeroline=False)
         fig.update_yaxes(showgrid=False,zeroline=False)
+        fig.write_image(f"distplot for {i}.jpg")
         fig.show()
         # a.append(fig)
     df=data.drop("Loan Status",axis=1)
-    y=df.corr().columns.tolist()
-    z=df.corr().values.tolist()
+    data_for_corr = df.select_dtypes(include=["number"])
+    df_for_corr = pd.DataFrame(data_for_corr)
+    col=list(df_for_corr.columns)
+    y=df_for_corr.corr().columns.tolist()
+    z=df_for_corr.corr().values.tolist()
     z_text = np.around(z, decimals=4) # Only show rounded value (full value on hover)
     fig = ff.create_annotated_heatmap(z,x=y,y=y,annotation_text=z_text,colorscale=px.colors.sequential.Cividis_r,showscale=True)
     fig.update_layout(template='plotly_dark')
